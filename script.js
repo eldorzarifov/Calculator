911a4d144eedde2fed54525dd1dd30f7f0f0c3e7
eldorzarifov <eldorbekzarifov5@gmail.com>
2025-11-29 12:49:27 +0500

    V1.0 FINISH CSS, HTML AND BASIC FUNCTIONS HTML is finished by adding all the necessary content. CSS is finished by adding all the necessary styles. Some basic math functions added to Js file

---
+++ script.js
@@ -0,0 +1,31 @@
+function addNum(a, b){
+    return a + b;
+}
+
+function subtractNum(a, b){
+    return a - b;
+}
+
+function multiplyNum(a, b){
+    return a * b;
+}
+
+function divideNum(a, b){
+    return a / b;
+}
+
+function percentage(a, b){
+    return (a/100)*b;
+}
+function squareRootNum(a, b){
+    return a ** (1/b);
+}
+
+function powerNum(a, b){
+    return a ** b;
+}
+
+function plusminusNum(a){
+    return -a;
+}
+
