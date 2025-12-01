0fe441b2d087199fb3a9fc74d7bd4ae4809ef50b
eldorzarifov <eldorbekzarifov5@gmail.com>
2025-12-01 13:33:50 +0500

    V1.1 FINISH BASIC MATH AND CHAINING, TWEAK CSS, HTML Fixed the size of display of calculator Added classes to work with JS logic Wrote logic that makes calculator fully usable except ".", "backspace", "+/-" buttons

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function divideNum(a, b){
 function percentage(a, b){
     return (a/100)*b;
 }
-function squareRootNum(a, b){
+function rootNum(a, b){
     return a ** (1/b);
 }
 
@@ -25,7 +25,107 @@ function powerNum(a, b){
     return a ** b;
 }
 
-function plusminusNum(a){
+function plusMinusNum(a){
     return -a;
 }
 
+function operate(a, op, b){
+    if(op == "+"){
+        return addNum(a, b);
+    }
+    if(op == "−"){
+        return subtractNum(a, b);
+    }
+    if(op == "÷"){
+        if(b === 0){
+            return "Nope"
+        }
+        return divideNum(a, b);
+    }
+    if(op == "×"){
+        return multiplyNum(a, b);
+    }
+    if(op == "%"){
+        return percentage(a, b);
+    }
+    if(op == "√"){
+        if(b === 0){
+            return "Nope"
+        }
+        return rootNum(a, b);
+    }
+    if(op == "^"){
+        return powerNum(a, b);
+    }
+}
+
+function clearTBox (){
+    return textbox.textContent = "";
+}
+function reset(){
+    firstNum = null;
+    secondNum = null;
+    currentOperator = null;
+    resetDisplay = false;
+    textbox.textContent = "";
+}
+
+let firstNum = null;
+let secondNum = null;
+let currentOperator = null;
+let resetDisplay = false;
+
+const textbox = document.querySelector(".textbox");
+const numberButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const backspace = document.querySelector(".backspace");
+const AC = document.querySelector(".clear");
+const equalsBtn = document.querySelector(".equals");
+
+numberButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if(resetDisplay == true){
+            clearTBox();
+            resetDisplay = false;
+        }
+        textbox.textContent += btn.textContent;
+    })
+});
+
+operatorButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if(firstNum == null){
+            firstNum = Number(textbox.textContent);
+            currentOperator = btn.textContent;
+            resetDisplay = true;
+        }
+        else if(resetDisplay == false && firstNum !== null && currentOperator !== null){
+            secondNum = Number(textbox.textContent);
+            let result = operate(firstNum, currentOperator, secondNum);
+            firstNum = result;
+            currentOperator = btn.textContent;
+            textbox.textContent = firstNum;
+            resetDisplay = true;
+        }
+        else if(currentOperator !== null && resetDisplay == true && firstNum !== null){
+            currentOperator = btn.textContent;
+            resetDisplay = true;
+        }
+    })
+});
+
+equalsBtn.addEventListener('click', () => {
+    if(firstNum !== null && resetDisplay == false && currentOperator !==null){
+        secondNum = Number(textbox.textContent);
+        clearTBox();
+        let result = operate(firstNum, currentOperator, secondNum);
+        console.log(result);
+        secondNum = null;
+        // currentOperator = null;
+        textbox.textContent = result;
+        firstNum = result;
+        resetDisplay = true;
+    }
+})
+
+AC.addEventListener("click", reset);
